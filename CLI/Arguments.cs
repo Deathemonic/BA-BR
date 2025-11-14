402b1dc3bbc2ebbf8a740c50a3772c419b5a56e7
Deathemonic <zinth.chan@tuta.io>
2025-11-14 22:46:13 +0800

    chore: updated cli help

--- CLI/Arguments.cs
+++ CLI/Arguments.cs
@@ -8,10 +8,10 @@ namespace BABU.CLI;
 public static class Arguments
 {
     /// <summary>
-    ///     Blue Archive - Mod Updater
+    ///     Blue Archive - Bundle Updater
     /// </summary>
-    /// <param name="modded">-m, Path to the modded asset bundle.</param>
-    /// <param name="patch">-p, Path to the patch asset bundle.</param>
+    /// <param name="modded">-m, Path to the modded assetbundle.</param>
+    /// <param name="patch">-p, Path to the patch assetbundle.</param>
     /// <param name="includeTypes">--include, Comma-separated list of asset types to include (e.g., "gameobject,transform").</param>
     /// <param name="excludeTypes">--exclude, Comma-separated list of asset types to exclude (e.g., "gameobject,transform").</param>
     /// <param name="onlyTypes">--only, Only allow these asset types to match (e.g., "mesh,texture2d").</param>
