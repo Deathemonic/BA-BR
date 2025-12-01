0958cff14fab8990aa0800d498aa6a44f78093e6
Deathemonic <zinth.chan@tuta.io>
2025-12-01 11:32:05 +0800

    fix: use apped to original resource instead of make a new one

--- BABU/Services/Bundle/BundleResourceService.cs
+++ BABU/Services/Bundle/BundleResourceService.cs
@@ -5,46 +5,82 @@ namespace BABU.Services.Bundle;
 
 public class BundleResourceService
 {
-    private const string CustomResourceName = "CAB-BABR.resource";
-    private readonly List<byte> _resourceData = [];
+    private readonly List<byte> _newResourceData = [];
     private readonly Dictionary<string, (long offset, long size)> _assetOffsets = [];
+    private string? _cachedArchivePath;
+    private long _existingResourceSize;
 
-    public (string resourcePath, long offset, long size) AddAsset(string assetName, byte[] data)
+    public (string resourcePath, long offset, long size) AddAsset(string assetName, byte[] data, BundleFileInstance bundleInst)
     {
-        var offset = _resourceData.Count;
-        _resourceData.AddRange(data);
+        if (_cachedArchivePath == null)
+        {
+            _cachedArchivePath = GetArchivePath(bundleInst);
+            _existingResourceSize = GetExistingResourceSize(bundleInst);
+        }
+
+        var offset = _existingResourceSize + _newResourceData.Count;
+        _newResourceData.AddRange(data);
         var size = data.Length;
 
         _assetOffsets[assetName] = (offset, size);
 
-        return (CustomResourceName, offset, size);
+        return (_cachedArchivePath, offset, size);
+    }
+
+    private static string GetArchivePath(BundleFileInstance bundleInst)
+    {
+        var existingResource = bundleInst.file.BlockAndDirInfo.DirectoryInfos
+            .FirstOrDefault(d => d.Name.EndsWith(".resource"));
+
+        if (existingResource != null)
+        {
+            var existingName = existingResource.Name;
+            var cabHash = existingName.Replace(".resource", "");
+            return $"archive:/{cabHash}/{existingName}";
+        }
+
+        return "archive:/CAB-unknown/CAB-unknown.resource";
+    }
+
+    private static long GetExistingResourceSize(BundleFileInstance bundleInst)
+    {
+        var existingResource = bundleInst.file.BlockAndDirInfo.DirectoryInfos
+            .FirstOrDefault(d => d.Name.EndsWith(".resource"));
+
+        return existingResource?.DecompressedSize ?? 0;
     }
 
     public void WriteToBundle(BundleFileInstance bundleInst)
     {
-        if (_resourceData.Count == 0)
+        if (_newResourceData.Count == 0)
             return;
 
         var bundle = bundleInst.file;
-        var resourceData = _resourceData.ToArray();
+        var existingResource = bundle.BlockAndDirInfo.DirectoryInfos
+            .FirstOrDefault(d => d.Name.EndsWith(".resource"));
 
-        var existingIndex = bundle.GetFileIndex(CustomResourceName);
-        if (existingIndex != -1)
-        {
-            var existingDirInfo = bundle.BlockAndDirInfo.DirectoryInfos[existingIndex];
-            existingDirInfo.Replacer = new ContentReplacerFromBuffer(resourceData);
-        }
-        else
-        {
-            var newDirInfo = new AssetBundleDirectoryInfo
-            {
-                Name = CustomResourceName,
-                DecompressedSize = resourceData.Length,
-                Flags = 0x04,
-                Replacer = new ContentReplacerFromBuffer(resourceData)
-            };
+        if (existingResource == null)
+            return;
 
-            bundle.BlockAndDirInfo.DirectoryInfos.Add(newDirInfo);
+        var existingData = ReadExistingResourceData(bundleInst, existingResource);
+        var combinedData = new byte[existingData.Length + _newResourceData.Count];
+        
+        Array.Copy(existingData, 0, combinedData, 0, existingData.Length);
+        _newResourceData.CopyTo(combinedData, existingData.Length);
+
+        existingResource.Replacer = new ContentReplacerFromBuffer(combinedData);
+        existingResource.DecompressedSize = combinedData.Length;
+    }
+
+    private static byte[] ReadExistingResourceData(BundleFileInstance bundleInst, AssetBundleDirectoryInfo dirInfo)
+    {
+        var bundle = bundleInst.file;
+        var reader = bundle.DataReader;
+
+        lock (reader)
+        {
+            reader.Position = dirInfo.Offset;
+            return reader.ReadBytes((int)dirInfo.DecompressedSize);
         }
     }
 }
